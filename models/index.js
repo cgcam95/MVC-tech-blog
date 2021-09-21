3ff1c70d1cf2437638bb8083a892e362d04d3b36
Camil Grabowski <camil.grabowski@uconn.edu>
2021-09-21 17:56:36 -0400

    starter

---
+++ models/index.js
@@ -0,0 +1,35 @@
+const Comment = require('./Comment');
+const User = require('./User');
+const Post = require('./Post');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: 'CASCADE'
+});
+
+module.exports = {Comment, User, Post}
