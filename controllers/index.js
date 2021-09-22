16e10c46fbf51397db69512a3fdadbaa84444b66
Camil Grabowski <camil.grabowski@uconn.edu>
2021-09-22 12:44:40 -0400

    lots of errors

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
-
